b4f1a46bdf1118073cbf631c7369900959cda681
akromish <akram.weheba.11@gmail.com>
2020-08-22 02:56:24 -0400

    nc

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
-mongoose.connect(process.env.MONGODB_URI , { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/local-app', { useNewUrlParser: true });
 const connection = mongoose.connection;
 
 connection.once('open', function() {
