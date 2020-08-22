2b6d9c36c2ecff527e853b42d32bd5f8cd613b8c
akromish <akram.weheba.11@gmail.com>
2020-08-22 14:38:11 -0400

    added atlas whitelist, re delpoying

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bikeRoutes = express.Router();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 
 let Bike = require('./models/bike.model');
